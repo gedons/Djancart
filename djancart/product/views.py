5154c0d7f05bd4de1684bfd7b61def8d047f2977
Nerdy <gedoniani@gmail.com>
2023-01-31 11:41:10 +0100

    initial

---
+++ djancart/product/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from .models import Product, Category
+from .serializers import ProductSerializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class LatestProductList(APIView):
+	def get(self, request, format=None):
+		products = Product.objects.all()[0:4]
+		serializer = ProductSerializer(products, many=True)
+		return Response(serializer.data)
+		
